d145de91f8c3c871258f9893238f7006526b0f70
Edwin <mwitaedwin14@gmail.com>
2025-11-20 14:16:24 +0400

    made a create all tables section

--- app/data/schema.py
+++ app/data/schema.py
@@ -10,3 +10,11 @@ def create_users_table(conn):
     )
 """)
     conn.commit()
+
+def create_all_tables(conn):
+"""Create all tables"""
+    create_users_table(conn)
+    create_cyber_incidents_table(conn)
+    create_datasets_metadata_table(conn)
+    create_it_tickets_table(conn)
+    
