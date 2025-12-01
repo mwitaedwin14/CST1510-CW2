bfab7e654dca332a81a68b45e4338bad79aa7469
Edwin <mwitaedwin14@gmail.com>
2025-12-01 16:51:31 +0400

    added user_table to db.py

--- CW2_M01036583_CST1510/app/data/db.py
+++ CW2_M01036583_CST1510/app/data/db.py
@@ -13,3 +13,29 @@ def connect_database(db_path=DB_PATH):
            sqlite3.Connection: Database connection object
     """
     return sqlite3.connect(str(db_path))
+
+def create_users_table(conn):
+    """
+    Create the users table if it doesn't exist.
+
+    This is a COMPLETE IMPLEMENTATION as an example.
+    Study this carefully before implementing the other tables!
+
+    Args:
+        conn: Database connection object
+    """
+    cursor = conn.cursor()
+
+    #SQL statement to create users table
+    create_table_sql = """
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        password_hash TEXT NOT NULL,
+        role TEXT DEFAULT 'user', 
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    )
+    """
+    cursor.execute(create_table_sql)
+    conn.commit()
+    print("Users table created successfully!")
