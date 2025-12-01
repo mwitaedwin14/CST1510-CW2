8b82ea00303e0d70bdc6c88a56eb9ce272bbaeae
Edwin <mwitaedwin14@gmail.com>
2025-12-01 16:43:05 +0400

    changed the code on db.py

--- CW2_M01036583_CST1510/app/data/db.py
+++ CW2_M01036583_CST1510/app/data/db.py
@@ -4,5 +4,12 @@ from pathlib import Path
 DB_PATH = Path("DATA") / "intelligence_platform.db"
 
 def connect_database(db_path=DB_PATH):
-    """Connect to SQLite database."""
+    """Connect to SQLite database.
+       Creates the database file if it doesn't exist.
+
+       Args:
+           db_path: Path to the database file
+       Returns:
+           sqlite3.Connection: Database onnection object
+    """
     return sqlite3.connect(str(db_path))
