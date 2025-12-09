4d1f68087353c2435a35133670864c9ff2af5868
Edwin <mwitaedwin14@gmail.com>
2025-12-09 12:34:26 +0400

    removed comment on auth.py

--- CW2_M01036583_CST1510/app/data/auth.py
+++ CW2_M01036583_CST1510/app/data/auth.py
@@ -11,7 +11,7 @@ def hash_password(plain_text_password: str) -> str:
     password_bytes = plain_text_password.encode('utf-8')
     salt = bcrypt.gensalt()
     hashed_bytes = bcrypt.hashpw(password_bytes, salt)
-    return hashed_bytes.decode('utf-8')  # Store as string in file
+    return hashed_bytes.decode('utf-8') 
 
 
 def verify_password(plain_text_password: str, hashed_password: str) -> bool:
