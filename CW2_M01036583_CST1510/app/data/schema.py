31e43e875805edfd21656abb8b720511585f33fa
Edwin <mwitaedwin14@gmail.com>
2025-12-10 15:46:11 +0400

    added new code to schema.py

--- CW2_M01036583_CST1510/app/data/schema.py
+++ CW2_M01036583_CST1510/app/data/schema.py
@@ -2,13 +2,52 @@ def create_users_table(conn):
 """Create users table"""
     cursor = conn.cursor()
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL,
-    role TEXT DEFAULT 'user'
-    )
-""")
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT UNIQUE NOT NULL,
+            password_hash TEXT NOT NULL,
+            role TEXT DEFAULT 'user'
+        )
+    """)
+    # Cyber Incidents
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS cyber_incidents (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            date TEXT,
+            incident_type TEXT,
+            severity TEXT,
+            status TEXT,
+            description TEXT,
+            reported_by TEXT
+        )
+    """)
+    # Datasets Metadata
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS datasets_metadata (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            dataset_name TEXT NOT NULL,
+            category TEXT,
+            source TEXT,
+            last_updated TEXT,
+            record_count INTEGER,
+            file_size_mb REAL
+        )
+    """)
+    # IT Tickets
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS it_tickets (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            ticket_id TEXT UNIQUE NOT NULL,
+            priority TEXT,
+            status TEXT,
+            category TEXT,
+            subject TEXT NOT NULL,
+            description TEXT,
+            created_date TEXT,
+            resolved_date TEXT,
+            assigned_to TEXT
+        )
+    """)
     conn.commit()
 
 def create_all_tables(conn):
