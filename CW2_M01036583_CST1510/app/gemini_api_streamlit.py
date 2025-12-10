0475c56d59e199e7e9892c42f0394801ad9fef8f
Edwin <mwitaedwin14@gmail.com>
2025-12-10 19:32:45 +0400

    created gemini_api_streamlit.py

--- CW2_M01036583_CST1510/app/gemini_api_streamlit.py
+++ CW2_M01036583_CST1510/app/gemini_api_streamlit.py
@@ -1,13 +1,11 @@
 import streamlit as st
 from google import genai
 
-client = genai.Client(api=st.secrets[])
+client = genai.Client(api=st.secrets["AIzaSyC3A3G2aVL_891cbAUOnY-Obc9ylr8Ul_0"])
 st.subheader("Gemini API")
 
-
-
-
-
+response = client.models.generate_content("Explain how AI works in a few words.")
+print(response.text)
 
 
 #Display existing messages
