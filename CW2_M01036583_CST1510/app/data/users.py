850b279ab68b9cb8ea0a92e793356bfc3720d409
Edwin <mwitaedwin14@gmail.com>
2025-12-11 15:57:20 +0400

    Changed code on users.py

--- CW2_M01036583_CST1510/app/data/users.py
+++ CW2_M01036583_CST1510/app/data/users.py
@@ -1,23 +1,20 @@
+# app/data/users.py
+import sqlite3
 from app.data.db import connect_database
 
 def get_user_by_username(username):
-    """Retrieve user by its username"""
-    conn = connect_database.connect_database()
+    conn = connect_database()
     cursor = conn.cursor()
-    cursor.execute(
-        "SELECT * FROM users WHERE username = ?",
-        (username,)
-    )
+    cursor.execute("SELECT id, username, password_hash, role FROM users WHERE username = ?", (username,))
     user = cursor.fetchone()
     conn.close()
     return user
 
 def insert_user(username, password_hash, role='user'):
-    """Insert new user."""
-    conn = connect_database.connect_database()
+    conn = connect_database()
     cursor = conn.cursor()
-    cursor.execute()
-       "INSERT INTO users (username, password_hash, role) VALUES (?, ?, ?)"
+    cursor.execute(
+        "INSERT INTO users (username, password_hash, role) VALUES (?, ?, ?)",
         (username, password_hash, role)
     )
     conn.commit()
